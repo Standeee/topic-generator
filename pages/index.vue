e0357f2fe3cf7a06d6cbab5ba85c7952f0d986ae
Stan <stanhoekema@gmail.com>
2023-06-09 20:23:44 +0200

    Photo cards

--- pages/index.vue
+++ pages/index.vue
@@ -1,55 +1,72 @@
 <template>
-  <div class="wallet">
-    <div class="title">
-      <h1>Moederdag Cadeau</h1>
-      <p>J. Koster</p>
-    </div>
-    <div class="card">
-      <div class="logo" :class="{ logoDisabled: first}">
-        <img src="https://images.unsplash.com/photo-1638153534717-fb17b6d19040?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1335&q=80" alt="">
-      </div>
-      <div class="info">
-        <div class="qty">
-          <div>IPad Pro</div>
-          <div>Met Apple Pencil</div>
-        </div>
-        <div class="btn" :class="{ btnDisabled: first}" @click="openFirst">
-          Verzilveren
-        </div>
+  <div class="Container">
+    <!-- last card -->
+    <div class="Picture">
+      <img class="Picture-img" src="https://media.licdn.com/dms/image/C4D03AQHjPuY9oi3Www/profile-displayphoto-shrink_800_800/0/1579768452204?e=2147483647&v=beta&t=4ZgTgOkSEu2eKRoLWEVtgL8s2zYu80YMxI_0018U9Dk" alt="">
+      <div class="Picture-note">
+        <span>@DeyJordan - <a class="Network" href="https://codepen.io/DeyJordan" target="_top"><img src="https://cdn-icons-png.flaticon.com/512/2111/2111501.png" alt="CodePen"></a><a class="Network" href="https://twitter.com/DeyJordan" target="_top"><img src="https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-logo-vector-png-clipart-1.png" alt="twitter"></a></span>
       </div>
     </div>
-    <div class="card">
-      <div class="logo" :class="{ logoDisabled: second}">
-        <img src="https://images.unsplash.com/photo-1569629743817-70d8db6c323b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1198&q=80" alt="">
-      </div>
-      <div class="info">
-        <div class="qty">
-          <div>Verassingsbezoek</div>
-          <div>Van Niek en Stan</div>
-        </div>
-        <div class="btn" :class="{ btnDisabled: second}" @click="openSecond">
-          Verzilveren
-        </div>
+    <!-- other cards -->
+    <div class="Picture">
+      <img class="Picture-img" src="https://picsum.photos/id/733/400" alt="">
+      <div class="Picture-note">
+        <span>Over the clouds</span>
       </div>
     </div>
-    <div class="card">
-      <div class="logo" :class="{ logoDisabled: third}">
-        <img src="https://images.unsplash.com/photo-1510812431401-41d2bd2722f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="">
-      </div>
-      <div class="info">
-        <div class="qty">
-          <div>Wijnproeverij</div>
-          <div>Met Niek en Stan</div>
-        </div>
-        <div class="btn" :class="{ btnDisabled: third}" @click="openThird">
-          Verzilveren
-        </div>
+    <div class="Picture">
+      <img class="Picture-img" src="https://picsum.photos/id/613/400" alt="">
+      <div class="Picture-note">
+        <span>Golden Gate Bridge - San Francisco</span>
       </div>
     </div>
-    <div v-if="open" class="dialog">
-      <h1>{{ dialogTitle }}</h1>
-      <p>{{ dialogContent }}</p>
-      <span v-if="closeBtn" @click="close">x</span>
+    <div class="Picture">
+      <img class="Picture-img" src="https://picsum.photos/id/40/400" alt="">
+      <div class="Picture-note">
+        <span>Cat nose</span>
+      </div>
+    </div>
+    <div class="Picture">
+      <img class="Picture-img" src="https://picsum.photos/id/465/400" alt="">
+      <div class="Picture-note">
+        <span>Mountain</span>
+      </div>
+    </div>
+    <div class="Picture">
+      <img class="Picture-img" src="https://picsum.photos/id/1029/400" alt="">
+      <div class="Picture-note">
+        <span>Central Park - New York</span>
+      </div>
+    </div>
+    <div class="Picture">
+      <img class="Picture-img" src="https://picsum.photos/id/923/400" alt="">
+      <div class="Picture-note">
+        <span>Autumn</span>
+      </div>
+    </div>
+    <div class="Picture">
+      <img class="Picture-img" src="https://picsum.photos/id/425/400" alt="">
+      <div class="Picture-note">
+        <span>Coffee</span>
+      </div>
+    </div>
+    <div class="Picture">
+      <img class="Picture-img" src="https://picsum.photos/id/200/400" alt="">
+      <div class="Picture-note">
+        <span>An Irish cow enjoying the wind on the beach</span>
+      </div>
+    </div>
+    <div class="Picture">
+      <img class="Picture-img" src="https://picsum.photos/id/244/400" alt="">
+      <div class="Picture-note">
+        <span>pelicans at the water's edge</span>
+      </div>
+    </div>
+    <div class="Picture">
+      <img class="Picture-img" src="https://picsum.photos/id/15/400" alt="">
+      <div class="Picture-note">
+        <span>Waterfall</span>
+      </div>
     </div>
   </div>
 </template>
@@ -68,6 +85,60 @@ export default {
       sure: true,
       closeBtn: true
     }
+  },
+  mounted () {
+    const pictures = document.querySelectorAll('.Picture')
+    let previousTouch
+
+    function updateElementPosition (element, event) {
+      let movementX, movementY
+
+      if (event.type === 'touchmove') {
+        const touch = event.touches[0]
+        movementX = previousTouch ? touch.clientX - previousTouch.clientX : 0
+        movementY = previousTouch ? touch.clientY - previousTouch.clientY : 0
+        previousTouch = touch
+      } else {
+        movementX = event.movementX
+        movementY = event.movementY
+      }
+
+      const elementY = parseInt(element.style.top || 0) + movementY
+      const elementX = parseInt(element.style.left || 0) + movementX
+
+      element.style.top = elementY + 'px'
+      element.style.left = elementX + 'px'
+    }
+
+    function startDrag (element, event) {
+      const updateFunction = event => updateElementPosition(element, event)
+      const stopFunction = () => stopDrag({ update: updateFunction, stop: stopFunction })
+      document.addEventListener('mousemove', updateFunction)
+      document.addEventListener('touchmove', updateFunction)
+      document.addEventListener('mouseup', stopFunction)
+      document.addEventListener('touchend', stopFunction)
+    }
+
+    function stopDrag (functions) {
+      previousTouch = undefined
+      document.removeEventListener('mousemove', functions.update)
+      document.removeEventListener('touchmove', functions.update)
+      document.removeEventListener('mouseup', functions.stop)
+      document.removeEventListener('touchend', functions.stop)
+    }
+
+    pictures.forEach((picture) => {
+      const range = 100
+      const randomX = Math.random() * (range * 2) - range + (window.innerWidth / 2)
+      const randomY = Math.random() * (range * 2) - range + (window.innerHeight / 2)
+      const randomRotate = Math.random() * (range / 2) - range / 4
+      const startFunction = event => startDrag(picture, event)
+      picture.style.top = `${randomY}px`
+      picture.style.left = `${randomX}px`
+      picture.style.transform = `translate(-50%, -50%) rotate(${randomRotate}deg)`
+      picture.addEventListener('mousedown', startFunction)
+      picture.addEventListener('touchstart', startFunction)
+    })
   },
   methods: {
     close () {
@@ -127,204 +198,76 @@ export default {
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Caveat");
+
 body, html, #__nuxt, #__layout {
-  height: 100%;
   overflow: hidden;
-  font-family: 'Roboto';
-  margin: 0;
 }
 
-.title h1 {
-  margin: 0;
-  text-align: center;
-  color: white;
-}
-
-.title p {
-  margin: 0;
-  text-align: center;
-  color: white;
-  font-weight: bold;
-}
-
-.dialog {
+.Container {
   position: absolute;
   top: 50%;
-  left: calc(50% - 20px);
+  left: 50%;
   transform: translate(-50%, -50%);
-  background-color: white;
-  width: calc(100% - 20px);
-  margin: 20px;
-  padding: 20px;
-  border-radius: var(--radiusCard);
-  box-shadow: 0px 0px 0px 500px #00000052;
+  height: 100dvh;
+  width: 100vw;
+  overflow: hidden;
 }
 
-.dialog h1 {
-  margin: 0;
-  margin-bottom: 12px;
-  text-align: center;
-  color: black;
-}
-
-.dialog p {
-  margin: 0;
-  text-align: center;
-  color: black;
-  font-weight: bold;
-}
-
-.dialog span {
-  margin: 0;
-  text-align: center;
-  color: white;
-  background-color: var(--button);
-  border: 2px solid var(--button);
-  border-radius: 8px;
-  width: 28px;
-  height: 28px;
-  font-weight: bold;
-  font-size: 1.25rem;
+.Picture {
+  display: inline-block;
   position: absolute;
-  right: 10px;
-  top: 8px;
+  top: 0;
+  left: 0;
+  border: 5px solid #fff;
+  border-radius: 3px;
+  background: #fff;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+  transform: translate(-50%, -50%);
+  user-select: none;
+  cursor: pointer;
 }
 
-.hint {
-  border-radius: 4px;
-  padding: 4px;
-  font-weight: bold;
-  text-decoration: underline;
+.Picture-img {
+  display: block;
+  width: 300px;
+  height: 300px;
+  pointer-events: none;
 }
 
-.error {
-  color: red;
-  font-weight: 500;
-  font-size: 12px;
-}
-.form {
+.Picture-note {
   display: flex;
-  justify-content: center;
   align-items: center;
-  height: 100%;
-  width: 100%;
-  flex-direction: column;
-  padding: 12px;
+  justify-content: center;
+  width: 300px;
+  height: 70px;
+  padding: 12px 24px;
+  font-size: 1.5rem;
+  text-align: center;
+}
+
+.Network {
+  display: inline-block;
+  padding: 0 5px;
+}
+
+.Network img {
+  width: 1.5rem;
+  aspect-ratio: 1 / 1;
+  vertical-align: middle;
+}
+
+/* --------------------- */
+/* Other styles          */
+/* --------------------- */
+
+* {
   box-sizing: border-box;
 }
 
-:root {
-   --radiusCard : 15px   ;
-   --background : #fd8974;
-   --cardColor  : #343433;
-   --cardInfo   : #ecf0f1;
-   --money      : #7f8c8d;
-   --button     : #f03f3f;
-}
-
-@media only screen and (max-width: 280px) {
-  :root {
-    --background  : #e74c3c;
-  }
-}
-
-*{
-   box-sizing: border-box;
-   font-family: 'Archivo Black', sans-serif;
-}
-
-html,
-body{
-   margin: 0;
-   width: 100%;
-   height: 100%;
-   background: var(--background);
-}
-
-.wallet{
-   display: flex;
-   justify-content: center;
-   align-items: center;
-   flex-direction: column;
-   gap: 20px;
-   width: 100%;
-   height: 100%;
-   padding: 1rem;
-}
-
-.card {
-   display: grid;
-   grid-template-columns: auto 100px auto;
-   grid-template-rows: 3fr 1fr;
-   grid-template-areas:
-    "  .  logo   . "
-    "info info info";
-   background: var(--cardColor);
-   border-radius: var(--radiusCard);
-   max-width: 500px;
-   width: 100%;
-   height: 200px;
-   overflow: hidden;
-   box-shadow: 0px 0px 26px rgba(0,0,0,0.1);
-   transition: box-shadow .5s ease;
-}
-
-.card:hover {
-  box-shadow: 13px 15px 21px rgba(0,0,0,0.1);
-}
-
-.logo {
-   grid-column: 1 / 4;
-   grid-row: 1;
-   display: flex;
-   align-items: center;
-   height: 100%;
-   overflow: hidden;
-}
-
-.logo img {
-  width: 100%;
-}
-
-.info{
-   grid-area: info;
-   background: var(--cardInfo);
-   display: flex;
-   align-items: center;
-   justify-content: space-between;
-   padding: .8rem 1.5rem;
-}
-
-.btn {
-   padding: .8rem 1rem;
-   background: var(--button);
-   border-radius: 4px;
-   color: white;
-   cursor: pointer;
-   font-weight: bold;
-}
-
-.btnDisabled {
-  background-color: gray;
-}
-
-.logoDisabled {
-  filter: grayscale();
-}
-
-.qty{
-   line-height:1.25rem;
-}
-
-.qty div:nth-child(1){
-  font-size:1.2rem;
-  font-weight: bold;
-  color: var(--cardColor);
-}
-
-.qty div:nth-child(2){
-  color:var(--money);
-  font-weight: bold;
-  font-size: 0.9rem;
+body {
+  margin: 0;
+  font-family: 'Caveat', serif;
+  overflow: hidden;
 }
 </style>
